d6bb129cdd07f6294ced6678d27fd32215f236ec
Norman94 <ingnbh@gmail.com>
2016-08-08 17:42:35 -0600

    Agregar Metodo Primo

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 
+def es_primo(numero):
+    contador = 0
+    verificar = False
+    for i in range(1, numero + 1):
+        if (numero % i) == 0:
+            contador = contador + 1
+        if contador >= 3:
+            verificar = True
+            break
+
+    if contador == 2 or verificar == False:
+        print ("el numero es primo")
+    else:
+        print ("el numero no es primo")
 
 
 
 
 
-try:
-        numUsuario = int(input("Digite un numero:"))
-        if(numUsuario<=0):
-            print("Debe de digitar un numero entero")
-        elif(numUsuario==1):
-            print ("El numero es inpar y primo a la vez")
-        elif((numUsuario%2)==0):
-            print("par")
-        else:
-            print("Impar")
-except:
-        print("Debe de digitar un numero entero")
+def main():
+        try:
+                numUsuario = int(input("Digite un numero:"))
+                if(numUsuario<=0):
+                    print("Debe de digitar un numero entero")
+                elif(numUsuario==1):
+                    print ("El numero es impar y primo a la vez")
+                elif((numUsuario%2)==0):
+                    print("par")
+                else:
+                    print("Impar")
+                es_primo(numUsuario)
+
+        except:
+                print("Debe de digitar un numero entero")
 
 
-def es_primo(x):
 
+main()
 
-
-    return x
 
